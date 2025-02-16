c70a9bf86e0fd17d2676e82b4b395c2d7528cae6
user <dolmatova3010@yandex.ru>
2025-02-16 21:04:28 +0700

    test 2 push with nginx and docker

--- config/settings.py
+++ config/settings.py
@@ -140,11 +140,11 @@ TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 
-MEDIA_URL = "/media"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
